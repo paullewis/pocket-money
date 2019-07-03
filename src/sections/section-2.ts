5c8d5483f53caa59eeaddef2166edb2f49c78720
Paul Lewis <github@aerotwist.com>
2019-07-03 22:26:43 +0100

    Adds support for dynamic and static imports

---
+++ src/sections/section-2.ts
@@ -0,0 +1,8 @@
+import { clamp } from '../utils/clamp.js';
+
+export default {
+  go() {
+    const value = clamp(1, 10, 20);
+    console.log('Section 2', value);
+  }
+};
---
+++ src/utils/clamp.ts
@@ -0,0 +1,4 @@
+export function clamp(value: number, min = Number.NEGATIVE_INFINITY,
+                      max = Number.POSITIVE_INFINITY) {
+  return Math.max(min, Math.min(max, value));
+}
